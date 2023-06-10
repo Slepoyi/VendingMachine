beca364aa5fc9369b78ecb37acb60fe1f81dc08b
Slepoyi <zub.dayu@mail.ru>
2023-06-11 00:05:57 +0300

    Create viewmodels

---
+++ src/VendingMachine.UI/Models/CoinViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VendingMachine.UI.Models
+{
+    public class CoinViewModel
+    {
+        [Key]
+        public CoinValue Value { get; set; }
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
---
+++ src/VendingMachine.UI/Models/DrinkViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VendingMachine.UI.Models
+{
+    public class DrinkViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; } = null!;
+        [Range(0, int.MaxValue)]
+        public int Price { get; set; }
+        [Range(0, int.MaxValue)]
+        public int Amount { get; set; }
+        public string? PhotoUrl { get; set; }
+    }
+}
