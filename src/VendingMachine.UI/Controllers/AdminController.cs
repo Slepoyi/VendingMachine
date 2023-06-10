72cb1a4a7f9f6c8a43c9fc8ba730e30813466900
Slepoyi <zub.dayu@mail.ru>
2023-06-11 00:05:43 +0300

    Create controllers for admin and user

---
+++ src/VendingMachine.UI/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VendingMachine.UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ src/VendingMachine.UI/Controllers/DrinksController.cs
@@ -0,0 +1,40 @@
+﻿using BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using VendingMachine.UI.Models;
+using VendingMachine.UI.Options;
+
+namespace VendingMachine.UI.Controllers
+{
+    public class DrinksController : Controller
+    {
+        private readonly IDrinkService _drinkService;
+        private readonly SecretOptions _secretOptions;
+
+        //public DrinksController(IDrinkService drinkService, IOptions<SecretOptions> secretOptions)
+        //{
+        //    _drinkService = drinkService;
+        //    _secretOptions = secretOptions.Value;
+        //}
+
+        public IActionResult GetDrinks(string secretKey)
+        {
+            //if (secretKey == _secretOptions.Secret)
+            //    return RedirectToAction("Admin");
+            //var drinks = _drinkService.Drinks;
+            ViewBag.Balance = 0;
+            var drinks = Enumerable.Empty<DrinkViewModel>();
+            return View(drinks);
+        }
+
+        public void OrderDrink(int drinkId, IEnumerable<CoinViewModel> coins)
+        {
+
+        }
+
+        public void GetChange(int remainingMoney)
+        {
+
+        }
+    }
+}
