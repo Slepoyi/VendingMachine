09312b704474740d17b54ccff491d61f412bfde0
Slepoyi <zub.dayu@mail.ru>
2023-06-11 00:05:27 +0300

    Create secret options class

---
+++ src/VendingMachine.UI/Options/SecretOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace VendingMachine.UI.Options
+{
+    public class SecretOptions
+    {
+        public const string Section = "SecretOptions";
+
+        public string Secret { get; set; } = null!;
+    }
+}
