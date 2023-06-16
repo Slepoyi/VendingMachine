d5f5c0d4b1f07e457c2c1c302d5fa7e146f63da7
Slepoyi <zub.dayu@mail.ru>
2023-06-16 20:54:00 +0300

    Add services

--- src/VendingMachine.UI/Program.cs
+++ src/VendingMachine.UI/Program.cs
@@ -1,6 +1,35 @@
+using BLL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using VendingMachine.BLL.ChangerServices;
+using VendingMachine.BLL.DataServices;
+using VendingMachine.BLL.Interfaces;
+using VendingMachine.DAL.Context;
+using VendingMachine.DAL.Interfaces;
+using VendingMachine.DAL.Repositories;
+using VendingMachine.UI.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<EfDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DesktopConnection"));
+});
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<SecretOptions>(builder.Configuration.GetSection(SecretOptions.Section));
+
+
+
+builder.Services.AddScoped<ICoinRepository, CoinRepository>();
+builder.Services.AddScoped<IDrinkRepository, DrinkRepository>();
+
+builder.Services.AddScoped<ICoinService, CoinService>();
+builder.Services.AddScoped<IDrinkService, DrinkService>();
+builder.Services.AddScoped<IChangerService, GreedyChangerService>();
+
+
 
 var app = builder.Build();
 
