e3a55f95806c4f32e696e6cf8038bc657b5a5ac7
Slepoyi <zub.dayu@mail.ru>
2023-06-16 20:54:14 +0300

    Add frontend logic

--- src/VendingMachine.UI/Controllers/DrinksController.cs
+++ src/VendingMachine.UI/Controllers/DrinksController.cs
@@ -10,9 +10,10 @@ namespace VendingMachine.UI.Controllers
     public class DrinksController : Controller
     {
         private readonly IDrinkService _drinkService;
+        private readonly ICoinService _coinService;
         private readonly SecretOptions _secretOptions;
         private readonly IChangerService _changerService;
-        private readonly Dictionary<CoinValue, int> _coins = new()
+        private static readonly Dictionary<CoinValue, int> _coins = new()
         {
             { CoinValue.One, 0 },
             { CoinValue.Two, 0 },
@@ -20,12 +21,7 @@ namespace VendingMachine.UI.Controllers
             { CoinValue.Ten, 0 }
         };
 
-        //public DrinksController(IDrinkService drinkService, IOptions<SecretOptions> secretOptions)
-        //{
-        //    _drinkService = drinkService;
-        //    _secretOptions = secretOptions.Value;
-        //}
-
+        [HttpGet]
         public IActionResult GetDrinks(string secretKey)
         {
             //if (secretKey == _secretOptions.Secret)
@@ -43,36 +39,66 @@ namespace VendingMachine.UI.Controllers
             return View(dr);
         }
 
-        public async Task<string> OrderDrinkAsync(int drinkId, IEnumerable<CoinViewModel> coins)
+        [HttpGet]
+        public IActionResult GetDrinksPartial()
+        {
+            //if (secretKey == _secretOptions.Secret)
+            //    return RedirectToAction("Main", "Admin");
+            //var drinks = _drinkService.Drinks;
+            var drinks = Enumerable.Empty<DrinkViewModel>();
+            var dr = drinks.ToList();
+            dr.Add(new DrinkViewModel
+            {
+                Id = 1,
+                Amount = 5,
+                Name = "Coffee",
+                Price = 7
+            });
+            return PartialView(dr);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OrderDrinkAsync(int drinkId)
         {
             var drink = await _drinkService.FindDrinkAsync(drinkId);
             if (drink is null)
-                return "Incorrect id";
+                return NotFound("Incorrect id");
 
             if (drink.Amount <= 0)
-                return "This drink is over";
+                return BadRequest("This drink is over");
 
             if (GetBalance() < drink.Price)
-                return "Not enough money";
+                return BadRequest("Not enough money");
 
             var extractTask = ExtractCoinsAsync(drink.Price);
             drink.Amount -= 1;
             var updateTask = _drinkService.UpdateDrinkAsync(drink);
             await Task.WhenAll(extractTask, updateTask);
-            return $"Here is your {drink.Name}! Enjoy!";
+            return Ok($"Here is your {drink.Name}! Enjoy!");
         }
 
+        [HttpGet]
         public async Task<IEnumerable<CoinViewModel>> GetChangeAsync(int remainingMoney)
         {
             var change = await _changerService.GetChangeAsync(remainingMoney);
             return CoinDictToEnumerable.Convert(change);
         }
 
-        public void AddCoin(CoinValue coin)
+        [HttpPost]
+        public void AddCoin(CoinValue value)
         {
-            _coins[coin] += 1;
+            if (!_coins.ContainsKey(value))
+                return;
+
+            //var coinDto = await _coinService.FindCoinAsync(value);
+
+            //if (coinDto is null || !coinDto.IsAccepted)
+            //    return;
+
+            _coins[value] += 1;
         }
 
+        [HttpGet]
         public int GetBalance()
         {
             return _coins.Sum(c => (int)c.Key * c.Value);
