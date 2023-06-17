290b57fbf32169639ab670283c0f844eeccffc59
Slepoyi <zub.dayu@mail.ru>
2023-06-17 19:29:40 +0300

    Renamed classes

--- src/VendingMachine.UI/Controllers/CustomerController.cs
+++ src/VendingMachine.UI/Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 ﻿using BLL.Dtos;
 using BLL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using VendingMachine.BLL.Interfaces;
 using VendingMachine.UI.Extensions;
 using VendingMachine.UI.Models;
@@ -8,42 +9,45 @@ using VendingMachine.UI.Options;
 
 namespace VendingMachine.UI.Controllers
 {
-    public class DrinksController : Controller
+    public class CustomerController : Controller
     {
         private readonly IDrinkService _drinkService;
         private readonly ICoinService _coinService;
-        private readonly SecretOptions _secretOptions;
         private readonly IChangerService _changerService;
+        private readonly SecretOptions _secretOptions;
         private static int _balance;
 
-        public DrinksController(IDrinkService drinkService, ICoinService coinService,
-            SecretOptions secretOptions, IChangerService changerService)
+        public CustomerController(IDrinkService drinkService, ICoinService coinService,
+            IChangerService changerService, IOptions<SecretOptions> secretOptions)
         {
             _drinkService = drinkService;
             _coinService = coinService;
-            _secretOptions = secretOptions;
             _changerService = changerService;
-            ViewBag.Balance = 0;
-            _balance = 0;
+            _secretOptions = secretOptions.Value;
         }
 
         [HttpGet]
-        public IActionResult GetDrinks(string secretKey)
+        [Route("/[controller]/main")]
+        public IActionResult Main(string secretKey)
         {
             if (secretKey == _secretOptions.Secret)
-                return RedirectToAction("Main", "Admin");
+                return RedirectToAction("main", "admin");
+            _balance = 0;
             var drinks = _drinkService.Drinks;
-            return View(drinks);
+            return View(drinks.ToDrinkViewModelEnumerable());
         }
 
         [HttpGet]
-        public IActionResult GetDrinksPartial()
+        [Route("/[controller]/drinkspartial")]
+        public IActionResult DrinksPartial()
         {
+            ViewBag.Balance = _balance;
             var drinks = _drinkService.Drinks;
-            return PartialView(drinks);
+            return PartialView(drinks.ToDrinkViewModelEnumerable());
         }
 
         [HttpPost]
+        [Route("/[controller]/orderdrink")]
         public async Task<IActionResult> OrderDrinkAsync(int drinkId)
         {
             var drink = await _drinkService.FindDrinkAsync(drinkId);
@@ -64,6 +68,7 @@ namespace VendingMachine.UI.Controllers
         }
 
         [HttpGet]
+        [Route("/[controller]/getchange")]
         public async Task<IEnumerable<CoinViewModel>> GetChangeAsync()
         {
             var change = await _changerService.GetChangeAsync(_balance);
@@ -72,32 +77,26 @@ namespace VendingMachine.UI.Controllers
         }
 
         [HttpPost]
+        [Route("/[controller]/addcoin")]
         public async Task AddCoinAsync(CoinValue value)
         {
             var coinDto = await _coinService.FindCoinAsync(value);
-
             if (coinDto is null || !coinDto.IsAccepted)
                 return;
 
             _balance += (int)value;
 
-            await _coinService.AddCoinsAsync(
-                new CoinDto[]
+            await _coinService.AddCoinAsync(
+                new CoinDto
                 {
-                    new CoinDto
-                    {
-                        Value = value,
-                        IsAccepted = true,
-                        Quantity = 1
-                    }
+                    Value = value,
+                    IsAccepted = true,
+                    Quantity = 1
                 });
         }
 
         [HttpGet]
-        public int GetBalance()
-        {
-            ViewBag.Balance = _balance;
-            return _balance;
-        }
+        [Route("/[controller]/getbalance")]
+        public int GetBalance() => _balance;
     }
 }
