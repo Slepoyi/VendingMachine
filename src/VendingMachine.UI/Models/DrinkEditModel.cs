565fb0923becb7b1ade4fee670956da32a6c2124
Slepoyi <zub.dayu@mail.ru>
2023-06-19 17:53:03 +0300

    Add drink edit model

---
+++ src/VendingMachine.UI/Models/DrinkEditModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VendingMachine.UI.Models
+{
+    public class DrinkEditModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; } = null!;
+        [Range(0, int.MaxValue)]
+        public int Price { get; set; }
+        [Range(0, int.MaxValue)]
+        public int Amount { get; set; }
+
+        public byte[]? Photo { get; set; }
+        public IFormFile? PhotoFile { get; set; }
+    }
+}
